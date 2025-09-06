d63b9110531226bddf826d0118ba5df737b1ccd7
Manolya Tamimi <manolya.tamimi@sadasol.com>
2025-09-06 20:59:12 +0300

    Updating routes

--- index.ts
+++ index.ts
@@ -9,37 +9,41 @@ const port = 3001;
 app.use(express.json())
 
 app.get("/characters", (req: express.Request, res: express.Response) => {
-  fs.readFile('characters.json', 'utf-8', (err, data) => {
-    if (err) {
-      console.error('something went wrong', err);
-      res.status(500).end();
-    } else {
+  try {
+    const data = fs.readFileSync('characters.json', 'utf-8')
+
+    if (data) {
       res.contentType('application/json').status(200).send(data);
+    } else {
+      console.error('something went wrong');
+      res.status(500).end();
     }
-  })
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error")
+  }
 })
 
 app.get("/characters/:id", (req: express.Request, res: express.Response) => {
   const id = req.params?.id;
-  fs.readFile('characters.json', 'utf-8', (err, data) => {
-    if (err) {
-      console.error('something went wrong', err);
-      res.status(500).end();
-    }
-    else {
-      const arr = JSON.parse(data) as character[];
-      if (id) {
-        const found = arr.find(item => item.id === +id);
-        if (found) {
-          res.contentType('application/json').status(200).send(found);
-        } else {
-          res.status(404).send("No characters found");
-        }
+  try {
+    const data = fs.readFileSync('characters.json', 'utf-8')
+
+    const arr = JSON.parse(data) as character[];
+    if (id) {
+      const found = arr.find(item => item.id === +id);
+      if (found) {
+        res.contentType('application/json').status(200).send(found);
       } else {
-        res.status(400).send("Something went wrong");
+        res.status(404).send("No characters found");
       }
+    } else {
+      res.status(400).send("Something went wrong");
     }
-  })
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error")
+  }
 })
 
 app.use((req: express.Request, res: express.Response) => {
